26f178f73c5d13afc9800ff04de5f552b986b5f9
devstack99 <belkhaouni.raouf@gmail.com>
2018-04-20 17:58:52 +0200

    Friday Commit

---
+++ PyCharmProjects/TP_Python/exo07.py
@@ -0,0 +1,16 @@
+num = int((input("Enter A number Between 10 and 20 : ")))
+while num  in range(10,20):
+    print("Yuppi ! ")
+elif exo07.py
+
+
+
+if num > 20:
+
+if num < 10:
+    print("Plus Grand ! ")
+    exo6()
+if num >= 10:
+
+if num < 20:
+    print("Yuppi ! ")
